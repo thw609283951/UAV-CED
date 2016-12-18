46a0fb98cabaf0de1746cfa8a69a70996aef39e4
Ma Yifeng <993030067@qq.com>
2016-12-18 19:57:35 +0800

    build frame in detail

--- src/UAV/service/ExpressPathArrangeService.java
+++ src/UAV/service/ExpressPathArrangeService.java
@@ -7,18 +7,56 @@ import java.util.List;
 
 import UAV.entity.CarPath;
 import UAV.entity.ChildZone;
+import UAV.entity.DockPoint;
+import UAV.entity.NeedPoint;
+import UAV.entity.Point;
+import UAV.entity.WarePoint;
 
 public class ExpressPathArrangeService {
+
+	
+	private List<DockPoint> allDockPoints;
+	
+	private List<NeedPoint> allNeedPoints;
+	private List<DockPoint> selectedDockPoints;
+	private List<WarePoint> warePoints;
+	public List<DockPoint> getAllDockPoints() {
+		return allDockPoints;
+	}
+
+
+	public void setAllDockPoints(List<DockPoint> allDockPoints) {
+		this.allDockPoints = allDockPoints;
+	}
+
+
+	public List<NeedPoint> getAllNeedPoints() {
+		return allNeedPoints;
+	}
+
+
+	public void setAllNeedPoints(List<NeedPoint> allNeedPoints) {
+		this.allNeedPoints = allNeedPoints;
+	}
+
+	
 
 	/**
 	 * 路径规划总算法
 	 */
 	public void pathArrange() {
+		
+		//TODO: 利用DAOimpl初始化allDockPoints,allNeedPoints,warePoints;
+		
 		pointPreProcess();
 		List<ChildZone> childZones = childZonePatition();
 		for (ChildZone childZone : childZones) {
-			CarPath carPath = carPathArrange(childZone);
-			UAVArrange(carPath, childZone);
+			//TODO:czPs是子区域的所有停靠点和对应的仓库点，需要使用childZone从数据库获得
+			List<Point> czPs = new ArrayList<Point>();
+			
+			double[][] pDis = getPointDis(czPs);
+			List<Point> carPath = carPathArrange(czPs, pDis);
+			UAVArrange(carPath);
 		}
 	}
 	
@@ -31,15 +69,29 @@ public class ExpressPathArrangeService {
 		
 	}
 	
+	
 	/**
+	 * 马
+	 * 
 	 * 此子函数为距离计算模块和停靠点选取模块的综合
+	 * 负责填写need_point表中的dockid和dockdis
+	 * 在allNeedPoints中修改并将修改添加到数据库
+	 * 并将allDockPoints中选出selectedDockPoints
+	 * 并释放allDockPoints
 	 */
 	private void pointPreProcess() {
 		
 	}
 	
+	
+	
 	/**
+	 * 黄
+	 * 
 	 * 子区域划分模块
+	 * 负责填写dock_point表中的czid
+	 * 在selectedDockPoints中修改并将修改添加到数据库
+	 * 生成ChildZone序列，并填写其中的wrid
 	 */
 	private List<ChildZone> childZonePatition() {
 		
@@ -48,18 +100,36 @@ public class ExpressPathArrangeService {
 	}
 	
 	/**
-	 * 解决旅行商问题的模块,返回形式需要再思考一下
+	 * 冯
+	 * 
+	 * 解决旅行商问题的模块,返回形式为最优解的行走序列，第一个点为仓库点，之后顺序的为停靠点
 	 * @param childZone
 	 */
-	private CarPath carPathArrange(ChildZone childZone) {
+	private List<Point> carPathArrange(List<Point> czPs, double[][] pDis) {
 		
-		return new CarPath();
+		return new ArrayList<Point>();
 	}
 	
 	/**
+	 * 妥
 	 * 无人机调度模块
+	 * @param carPath 已经经过计算的旅行商问题最优解的序列，车要按着这个序列走
 	 */
-	private void UAVArrange(CarPath carPath, ChildZone childZone) {
-		
+	private void UAVArrange(List<Point> carPath) {
+		//TODO:会需要从停靠点得到停靠点所负责的所有需求点的集合
 	}
+	
+	/**
+	 * 马
+	 * 计算points数组中各个点之间的距离。以二维数组形式返回
+	 * @param points
+	 * @return
+	 */
+	private double[][] getPointDis(List<Point> points) {
+		
+		double[][] a = null;
+		return a;
+	}
+	
+	
 }
